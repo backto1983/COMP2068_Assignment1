7bf2d36d4101c231b917c415142e429296742fd7
backto1983 <200358165@student.georgianc.on.ca>
2018-06-06 14:15:31 -0400

    Added controller; contains exports for homepage and contact

---
+++ controllers/indexController.js
@@ -0,0 +1,14 @@
+exports.homePage = (req, res, next) => {
+    res.render('index', { 
+      title: 'Assignment 1 - Express Portfolio Website',
+      message: 'Welcome to my personal portfolio website built using Express.js and implementing the EJS templating engine. Site deployment is done via GitHub and Heroku.' });
+  };
+  
+  exports.contact = (req, res, next) => {
+    const message = 'blablabla';
+    res.render('contact', { 
+      title: 'Assignment 1 - Express Portfolio Website' });
+  };
+  
+
+  
