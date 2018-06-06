3259ca79cc55c570376ff738e8ee5fff5d69b263
backto1983 <200358165@student.georgianc.on.ca>
2018-06-06 16:07:06 -0400

    Included navbar to header and functions to every page in the controller; changes to CSS

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -4,10 +4,32 @@ exports.homePage = (req, res, next) => {
       message: 'Welcome to my personal portfolio website built using Express.js, Materialize front-end framework and implementing the EJS templating engine. Site deployment is done via GitHub and Heroku.' });
   };
   
+  exports.about = (req, res, next) => {
+    const message = 'About';
+    res.render('about', { 
+      title: 'Contact Me',
+      message });
+  };
+
+  exports.projects = (req, res, next) => {
+    const message = 'Projects';
+    res.render('projects', { 
+      title: 'Some of my projects',
+      message });
+  };
+
+  exports.services = (req, res, next) => {
+    const message = 'Some of my services';
+    res.render('services', { 
+      title: 'Contact Me',
+      message });
+  };
+
   exports.contact = (req, res, next) => {
     const message = 'blablabla';
     res.render('contact', { 
-      title: 'Assignment 1 - Express Portfolio Website' });
+      title: 'Contact Me',
+      message });
   };
   
 
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,9 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', indexController.homePage);
+router.get('/about', indexController.about);
+router.get('/projects', indexController.projects);
+router.get('/services', indexController.services);
 router.get('/contact', indexController.contact);
 
 module.exports = router;
